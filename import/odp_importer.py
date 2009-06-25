e61a3877d1cfac6608627c30d97fa4fc38f2bab7
Kevin Roy <kiniou@kiniou-desktop.(none)>
2009-06-25 14:27:27 +0200

    Road to get ODP Styles working (3)
        * Added reportlab to get layout coordinates and paragraph break (still need a lot of work...)
        * Need to find a good font size conversion formula

--- import/odp_importer.py
+++ import/odp_importer.py
@@ -24,9 +24,31 @@ import sys
 import os
 import math
 
+import urllib
+
+import ImageFont,ImageFile,ImageDraw,Image
+
 from odf.opendocument import load,OpenDocumentPresentation
 from odf import text,presentation,draw,style,office
 
+from reportlab.platypus.flowables import Flowable, PTOContainer, KeepInFrame
+from reportlab.platypus.paragraph import Paragraph
+from reportlab.platypus.frames import Frame
+from reportlab.lib.styles import getSampleStyleSheet, ParagraphStyle
+from reportlab.lib.units import *
+from reportlab.platypus.doctemplate import PageTemplate, BaseDocTemplate, FrameBreak , PageBreak , NextPageTemplate
+from reportlab.pdfgen import canvas
+
+from reportlab import rl_config
+from reportlab.lib.enums import *
+from reportlab.pdfbase import pdfmetrics
+from reportlab.pdfbase.ttfonts import TTFont
+
+#rl_config.shapeChecking = 0
+#rl_config.overlapAttachedSpace= 0
+
+
+global_fonts = {}
 
 class BuildContext():
     
@@ -47,6 +69,10 @@ class BuildContext():
         self.font_list = {}
         self.load_fonts()
 
+        #FIXME : Crappy hack because I run out of time :'(
+        global global_fonts 
+        global_fonts= self.font_list.copy()
+
         self.blender = {}
         self.game = {}
         self.init_blender_file()
@@ -65,10 +91,12 @@ class BuildContext():
 
         self.blender['text'] = Text3d.New("tmp_text") #Create the Text3d object
         self.blender['text'].setExtrudeDepth(0)    #Give some relief 
-        self.blender['text'].setDefaultResolution(1)
+        self.blender['text'].setDefaultResolution(4)
+        self.blender['text'].setSpacing(0.92)
+        
         self.blender['text_object'] = self.blender['scene'].objects.new(self.blender['text'])
         self.blender['scene'].objects.unlink(self.blender['text_object'])
-        print dir(self.blender['text'])
+#        print dir(self.blender['text'])
         self.ratio_x = float( self.screen['width'] / ( self.screen['width'] % self.screen['height'] ) )
         self.ratio_y = float( self.screen['height'] / ( self.screen['width'] % self.screen['height'] ) )
 
@@ -122,14 +150,14 @@ class ODP_Text():
         self.attributes = x_text.attributes
         self.id = x_text.getAttribute('id')
         self.style_name = x_text.getAttribute( 'stylename' )
+        self.type = None
         if self.style_name == None:
             self.style_name = x_parent.style_name
         #    print "Text Style = %s" % self.style_name
         #else:
         #    print "Text Style = %s , Parent Style = %s" % (self.style_name , x_parent.style_name)
 
-        #FIXME : the text may be contained in another text element :/ ... Fu#!@&ing XML
-        self.text = x_text.firstChild
+            
 
         self.cond_style_name = x_text.getAttribute( 'condstylename' )
         self.class_names = x_text.getAttribute( 'classnames' )
@@ -139,8 +167,26 @@ class ODP_Text():
         while (parent.tagName != 'draw:text-box') :
             if ( parent.parentNode.tagName == 'text:list-item') :
                 self.level += 1
+            if ( parent.parentNode.tagName == 'text:list') :
+                self.type = 'list'
             parent = parent.parentNode
 
+        #FIXME : the text may be contained in another text element :/ ... Fu#!@&ing XML
+        tmp_text = x_text
+        while(tmp_text != None):
+            if hasattr(tmp_text.firstChild,'data'):
+                x_text = tmp_text.firstChild.data
+                break
+            tmp_text = tmp_text.firstChild
+
+        if (tmp_text == None) : x_text = ''
+
+        if (self.type == None):
+            self.text = x_text
+        elif (self.type == 'list'):
+            #FIXME : replace bullet character from the corresponding style
+            self.text = '- ' + x_text
+#            print repr(self.text) , dir(self.text)
 
 #        doc.getStyleByName(self.style_name).attributes
         
@@ -208,7 +254,7 @@ class ODP_Text():
             self.font['font-style'] = 'Regular'
 
 
-        self.font['font-size'] = i_font_size.replace("pt","")
+        self.font['font-size'] = float(i_font_size.replace("pt",""))
 
         if i_font_underline != 'none':
             self.font['font-underline'] = True
@@ -224,44 +270,113 @@ class ODP_Text():
         elif (i_real_style[1] == 'Regular') : self.font['real-style'] = i_real_style[0]
         else :self.font['real-style'] = " ".join(i_real_style)
 
-        #print self.font
+        print self.font
         #print ""
+
+        self.paragraph = None
+
+        styleSheet = getSampleStyleSheet()
+
+        self.styleSheet = styleSheet['BodyText']
+
+        self.styleSheet.fontSize = self.font['font-size']
+        if self.font['text-align'] == Text3d.LEFT:
+            self.styleSheet.alignment = TA_LEFT 
+        elif self.font['text-align'] == Text3d.RIGHT:
+            self.styleSheet.alignment = TA_RIGHT
+        elif self.font['text-align'] == Text3d.MIDDLE:
+            self.styleSheet.alignement = TA_CENTER
+
+
+        pdfmetrics.registerFont(TTFont(self.font['font-family'],global_fonts[self.font['font-family']][self.font['real-style']]['path']))
+
+        self.styleSheet.fontName = self.font['font-family']
+
+    def create_rl_text(self , story):
+        
+        self.paragraph = Paragraph(self.text.encode('utf-8'),self.styleSheet)
+        return self.paragraph
+
+    def debug_rl_story(self):
+        #print { 'width':self.paragraph.width/cm , 'height':self.paragraph.height/cm , 'text':self.paragraph.text}
+        print self.paragraph._fixedWidth , self.paragraph._fixedHeight 
+        lines = []
+        for l in self.paragraph.blPara.lines:
+            lines.append(" ".join(l[1]))
+        
+#        self.font['font-size'] = self.paragraph.blPara.fontSize
+
+#        print self.font
+
+        self.text = urllib.quote("\n".join(lines) )
+#        print urllib.unquote(self.text)
 
     def build( self , build_context) :
         build_context.doc['current_line'] += 1
         build_context.doc['current_text'] += 1
         i_page = build_context.doc['current_page']
         i_frame = build_context.doc['current_frame']
+        i_frame_layout = build_context.doc['frame_layout']
         i_element = build_context.doc['current_element']
         i_element_type = build_context.doc['current_element_type']
         i_line = build_context.doc['current_line']
+        i_line_offset = build_context.doc['line_offset']
         i_text = build_context.doc['current_text']
-
-        i_name = "Text_%d" % ( i_text )
-        #print self.text.data.encode('utf-8')
-        i_text = self.text.data.encode('utf-8')
-
+        
         i_ratio_x = build_context.ratio_x
         i_ratio_y = build_context.ratio_y
-        if self.font['text-align'] == Text3d.LEFT : i_start_x = -i_ratio_x/2.0
-        elif self.font['text-align'] == Text3d.RIGHT : i_start_x = i_ratio_x/2.0
-        else : i_start_x = 0
+
+        i_font_size = (self.font['font-size'] * 0.01)
+        print i_font_size
+        build_context.doc['line_offset'] += (i_font_size )
+        i_name = "Text_%d" % ( i_text )
+        #print self.text.data.encode('utf-8')
+        #i_text = str(self.text.encode('utf-8'))
+
+        #i_text = self.text.encode('utf-8')
+        i_text = urllib.unquote(self.text)
+
+        #Calculation of base coordinates for the text
+
+#        if self.font['text-align'] == Text3d.LEFT : i_start_x = -i_ratio_x/2.0
+#        elif self.font['text-align'] == Text3d.RIGHT : i_start_x = i_ratio_x/2.0
+#        else : i_start_x = 0
+#
+        i_start_x = -i_ratio_x/2.0
+        i_start_y = i_ratio_y/2.0
         
-        i_position_z = i_ratio_y/2.0 - 0.2 * ( i_line )
-        i_position_x = i_start_x + 1 * ( self.level - 1 )
+        i_frame_x = i_start_x + i_frame_layout['x']
+        i_frame_y = i_start_y - i_frame_layout['y']
+
+        i_position_z = i_frame_y - i_line_offset - i_font_size# - 0.2 * ( i_line )
+        i_position_x = i_frame_x + 0.2 * ( self.level - 1 )
         i_position_y = -0.01 #0.95 * ( i_page )
 
+#        i_image_font =  ImageFont.truetype(build_context.font_list[self.font['font-family']][self.font['real-style']]['path'] , size=int(self.font['font-size']) , encoding='unic')
+#        img = Image.new('RGB' , size=i_image_font.getsize(i_text))
+#        draw = ImageDraw.Draw(img)
+#        draw.text((0,0),i_text,font=i_image_font)
+#        img.save('%s.png' % i_name)
+#        print i_image_font.getsize(i_text)
+
         b_material = Material.New("Mat_%s" % i_name)
-        b_material.rgbCol = [1.0 , 0.0 , 0.0]
+        b_material.rgbCol = [0.5 , 0.5 , 0.5]
         b_material.setMode('Shadeless')
  
         b_text = build_context.blender['text']
+        
+        #print b_text.activeFrame , b_text.totalFrames
+        b_text.frameWidth = i_frame_layout['w']
+        b_text.frameHeight = i_font_size#i_frame_layout['h']
+        b_text.frameX = 0.0
+        b_text.frameY = 0.0 # i_line_offset - i_font_size
+
         b_text_object = build_context.blender['text_object']
         b_scene = build_context.blender['scene']
         #print ("%r" % (self.text.data), i_name, b_text.getSize())
         b_text.setText(i_text)   #Set the text for Text3d object
         b_text.setAlignment(self.font['text-align'])
-        b_text.setSize(0.2)
+        b_text.setSize(i_font_size)
         b_text.setFont(build_context.font_list[self.font['font-family']][self.font['real-style']]['blender'] ) #Set the font to Text3d object
         b_mesh = Mesh.New(i_name)
         
@@ -270,6 +385,7 @@ class ODP_Text():
 #        b_object.setMaterials([b_material])
 
         b_object = b_scene.objects.new(b_mesh)
+        b_scene.objects.new(b_text)
 
         b_object.RotX = math.pi/2        
         
@@ -287,8 +403,9 @@ class ODP_Text():
         str+="                   | condstylename : %s\n" % (self.cond_style_name)
         str+="                   | stylename     : %s\n" % (self.style_name)
         str+="                   | id            : %s\n" % (self.id)
-        str+="                   | text          : %s\n" % (unicode(self.text))
-        #str+="                   | attributes    : %s\n" % (self.attributes)
+        #str+="                   | text          : %s\n" % (unicode(self.text))
+        str+="                   | text          : %s\n" % (self.text)
+        str+="                   | attributes    : %s\n" % (self.attributes)
         str+="                   | level         : %s\n" % (self.level)
         str+="                   =====\n"
         
@@ -300,6 +417,20 @@ class ODP_TextBox() :
         self.style_name = parent.style_name 
 
         self.addTextsFrom( x_textbox , doc , styles)
+
+        self.paragraphs = [] 
+
+        
+    def create_rl_paragraphs(self , story):
+        
+        for i_text in self.texts :
+            self.paragraphs.append(i_text.create_rl_text(story))
+
+        return self.paragraphs
+
+    def debug_rl_story(self):
+        for i_text in self.texts :
+            i_text.debug_rl_story()
 
     def addTextsFrom( self, x_textbox , doc , styles) :
 
@@ -322,6 +453,7 @@ class ODP_TextBox() :
     def build( self , build_context) :
         build_context.doc['current_element'] += 1
         build_context.doc['current_element_type'] = "tb"
+        build_context.doc['line_offset'] = 0
         for n,i_text in enumerate(self.texts) :
             #build_context['line-number'] += 1
             i_text.build( build_context )
@@ -344,10 +476,10 @@ class ODP_Frame() :
         if x_frame.attributes.has_key('presentation:style-name'): self.style_name = x_frame.attributes['presentation:style-name']
         else : self.style_name = x_frame.attributes['draw:style-name']
         self.class_name = x_frame.getAttribute( 'class' )
-        self.width = x_frame.getAttribute( 'width' )
-        self.height = x_frame.getAttribute( 'height' )
-        self.x = x_frame.getAttribute( 'x' )
-        self.y = x_frame.getAttribute( 'y' )
+        self.width = float(x_frame.getAttribute( 'width' ).replace('cm',''))
+        self.height = float(x_frame.getAttribute( 'height' ).replace('cm',''))
+        self.x = float(x_frame.getAttribute( 'x' ).replace('cm',''))
+        self.y = float(x_frame.getAttribute( 'y' ).replace('cm',''))
         self.zindex = x_frame.getAttribute( 'zindex' )
         self.frame_elements = []
         
@@ -358,6 +490,36 @@ class ODP_Frame() :
         #if st_attr.has_key('style:parent-style-name') : str_attr
 
         self.addElementsFrom(x_frame , doc , styles)
+
+        self.rl_flowables = []
+        self.rl_frame = None
+        self.rl_keepinframe = None
+
+    def create_rl_frame( self, story , page):
+        
+        for i_element in self.frame_elements:
+            if isinstance(i_element,ODP_TextBox):
+                self.rl_flowables.extend(i_element.create_rl_paragraphs(story) )
+
+#        self.rl_flowables.append(FrameBreak())
+#        print self.rl_flowables
+
+
+        if len(self.rl_flowables) : 
+            self.rl_keepinframe = KeepInFrame(self.width * cm,self.height * cm,self.rl_flowables,mode='shrink')
+            story.append(self.rl_keepinframe)
+            story.append(FrameBreak())
+            self.rl_frame = Frame(self.x * cm, page.page_height*cm - self.y*cm - self.height* cm, self.width*cm, self.height*cm, leftPadding=0, bottomPadding=0 , rightPadding=0, topPadding=0)
+            self.rl_frame._oASpace
+        else :
+            self.rl_frame = None
+        return self.rl_frame
+
+    def debug_rl_story(self):
+        for i_element in self.frame_elements:
+            if isinstance(i_element,ODP_TextBox):
+                i_element.debug_rl_story()
+
 
     def addElementsFrom( self , x_frame , doc , styles) :
         
@@ -380,6 +542,21 @@ class ODP_Frame() :
         build_context.doc['current_element'] = 0
         build_context.doc['current_element_type'] = "None"
         build_context.doc['current_frame'] += 1
+
+
+        i_page_size = build_context.doc['page_size']
+        #print i_page_size
+        #FIXME : Rename ratio_x and ratio_y in b_page_width and b_page_height
+        i_ratio_x = build_context.ratio_x
+        i_ratio_y = build_context.ratio_y
+
+        i_frame_layout = {}
+        i_frame_layout['w'] = self.width / i_page_size['width'] * i_ratio_x
+        i_frame_layout['h'] = self.height/ i_page_size['height'] * i_ratio_y
+        i_frame_layout['x'] = self.x / i_page_size['width'] * i_ratio_x
+        i_frame_layout['y'] = self.y / i_page_size['height'] * i_ratio_y
+        build_context.doc['frame_layout'] = i_frame_layout
+        
         for i_element in self.frame_elements:
             i_element.build(build_context)
 
@@ -409,19 +586,44 @@ class ODP_Page() :
         self.name = ""
         self.frames = []
         self.number = 0
+        self.attributes = x_page.attributes
 
         self.name = x_page.getAttribute('name')
         self.masterpage = x_page.attributes['draw:master-page-name']
         
+        #self.layout
+
         if (len(styles['masterpages']) > 0) :
             self.layout = styles['masterpages'][self.masterpage].attributes['style:page-layout-name']
 
+        print self.layout
+
         if self.layout != None:
             prop = styles['pagelayouts'][self.layout].getElementsByType(style.PageLayoutProperties)[0]
-            self.page_width = prop.attributes['fo:page-width']
-            self.page_height = prop.attributes['fo:page-height']
+            self.page_width = float(prop.attributes['fo:page-width'].replace('cm',''))
+            self.page_height = float(prop.attributes['fo:page-height'].replace('cm',''))
 
         self.addFramesFrom(x_page , doc , styles)
+
+        self.rl_frames = []
+        self.rl_page = None
+
+    def create_rl_page( self , story):
+
+        for i_frame in self.frames:
+            rl_frame = i_frame.create_rl_frame(story,self)
+            if rl_frame!=None:
+                self.rl_frames.append(rl_frame)
+
+        self.rl_page = PageTemplate(self.name, self.rl_frames , pagesize=(self.page_width*cm,self.page_height*cm))
+
+#        story.append(PageBreak)
+        return self.rl_page
+
+    def debug_rl_story(self):
+        for i_frame in self.frames:
+            i_frame.debug_rl_story()
+
 
     def addFramesFrom( self , x_page , doc , styles) :
 
@@ -430,6 +632,7 @@ class ODP_Page() :
         while ( i_walk == True ) :
             #print i_element.tagName
             if ( i_element.tagName == 'draw:frame' ) :
+                #FIXME : get Frame order with PageLayout
                 self.frames.append( ODP_Frame(i_element , doc , styles) )
             if ( i_element.nextSibling != None ):
                 i_element = i_element.nextSibling
@@ -468,6 +671,7 @@ class ODP_Page() :
         str = ""
         str += "    |PAGE %s\n" % (self.name)
         str += "    | number %s\n" % (self.number)
+        str += "    | attributes %s\n" % (self.attributes)
         str += "    =====\n"
         for frame in self.frames :
             str += "%s" % (frame)
@@ -513,6 +717,38 @@ class ODP_Presentation() :
 #                        self.page_height = j.attributes['fo:page-height']
         
         self.addPagesFrom(x_presentation , doc , styles)
+
+        self.rl_doc = None
+        self.story = []
+        self.rl_pages = []
+
+        self.create_rl_doc()
+
+    def create_rl_doc(self):
+
+        for i_page in self.pages:
+            self.rl_pages.append( i_page.create_rl_page(self.story) )
+
+#        self.rl_doc = BaseDocTemplate("Test.pdf",
+#            pageTemplates = self.rl_pages,
+#            showBoundary = 1,
+#            verbose = False
+#            )
+
+        self.rl_doc = BaseDocTemplate("Test.pdf",
+            pageTemplates = self.rl_pages,
+            showBoundary = 1,
+            verbose = False
+            )
+
+        print self.story
+        #self.rl_doc.multiBuild(self.story,canvasMaker=canvas.Canvas(bottomup=1))
+        self.rl_doc.multiBuild(self.story)
+        self.debug_rl_story()
+
+    def debug_rl_story(self):
+        for i_page in self.pages:
+            i_page.debug_rl_story()
 
     def addPagesFrom( self, x_presentation , doc, styles) :
         
